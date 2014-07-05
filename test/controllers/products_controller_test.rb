079bf5a1d5177ddcd456a0c71915d4e79b9941dd
Joshua Langholtz <jjlangholtz@gmail.com>
2014-07-04 22:03:38 -0400

    Added shopping cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '#columns #side a', minimum: 4
   end
 
   test "should get new" do
